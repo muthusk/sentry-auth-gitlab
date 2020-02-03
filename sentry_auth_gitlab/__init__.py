2f80774c518ecc1d0bba3771c8ace1acd54e9dc7
Muthukumar Krithivasan SK <muthusk@users.noreply.github.com>
2020-02-02 18:08:00 -0800

    Update __init__.py

--- sentry_auth_gitlab/__init__.py
+++ sentry_auth_gitlab/__init__.py
@@ -1,16 +1,1 @@
 from __future__ import absolute_import
-
-import django
-
-if django.VERSION[:2] >= (1, 8):
-    # Django 1.9 specifically requires that models not be imported before setup,
-    # which sentry.auth does, so we need to use AppConfig here.
-    # Also works on 1.8.
-    default_app_config = "sentry_auth_gitlab.apps.Config"
-else:
-    # Provide backwards compatibility.
-    from sentry.auth import register
-
-from .provider import GitLabOAuth2Provider
-
-register('gitlab', GitLabOAuth2Provider)
