1e09c4c019fd99a7d312e5197aed54d6ba0118a7
Muthukumar Krithivasan SK <muthusk@users.noreply.github.com>
2020-02-02 18:09:16 -0800

    Update apps.py

--- sentry_auth_gitlab/apps.py
+++ sentry_auth_gitlab/apps.py
@@ -1,14 +1,11 @@
 from __future__ import absolute_import
-
 from django.apps import AppConfig
-
 
 class Config(AppConfig):
     name = "sentry_auth_gitlab"
-
+    
     def ready(self):
         from sentry import auth
-
         from .provider import GitLabOAuth2Provider
-
+        
         auth.register('gitlab', GitLabOAuth2Provider)
